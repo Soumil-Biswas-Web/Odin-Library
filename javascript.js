0b79321e460c7a19fd61494613918a292edc2af5
Soumil_Biswas <soumil.biswas19@gmail.com>
2024-04-08 09:55:29 +0530

    Added code for add_book and show_books

--- javascript.js
+++ javascript.js
@@ -1,4 +1,8 @@
+// Library Array
+
 const myLibrary = [];
+
+// Book Constructor
 
 function Book(name, writer, numberOfPages, isRead) {
     this.name = name;
@@ -17,18 +21,44 @@ function Book(name, writer, numberOfPages, isRead) {
     }
 }
 
+// Add book to Library
+
 function addBookToLibrary(Book) {
-    myLibrary.append(Book);
-    console.log(Book.name + " added to Library.")
+    myLibrary.push(Book);
+    console.log(Book.name + " added to Library.");
 }
+
+// Show All Books
 
 function showBooks(Book) {}
 
+// On Click add_book
+
+function onClickAddBook() {
+    document.querySelector(".add_book").style.display = "block";
+    document.querySelector(".show_books").style.display = "none";
+}
+
+// On Click show_books
+
+function onClickShowBooks() {
+    document.querySelector(".add_book").style.display = "none";
+    document.querySelector(".show_books").style.display = "block";
+}
+
 // Driver Code
 
-const theHobbit = new Book ("The Hobbit", "J.R.R. Tolkien", 295, false);
+/*
+const theHobbit = new Bo/ok ("The Hobbit", "J.R.R. Tolkien", 295, false);
 console.log(theHobbit.info());
 
 addBookToLibrary(theHobbit);
+*/
+
+const buttonAddBook = document.querySelector(".button_add_book");
+buttonAddBook.addEventListener("click", onClickAddBook);
+
+const buttonShowBooks = document.querySelector(".button_show_books");
+buttonShowBooks.addEventListener("click", onClickShowBooks);
 
 // Style the website like nhentai lol. Add light mode option as well.
